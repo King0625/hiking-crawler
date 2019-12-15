6f48f2c8c94a73fa190642f227220c61dc95ccec
kenchen <4915185-kenchen0625@users.noreply.gitlab.com>
2019-12-15 10:11:40 +0800

    Remove some comments

--- controllers/pttHikingController.js
+++ controllers/pttHikingController.js
@@ -10,34 +10,14 @@ exports.getFirstPage = (req, res, next) => {
     .then(totalPage => {
 
         const promises = [];
-        var i = 0
+        // P.S. 之後可用 recursion 濾掉特定日期之前的資料
         for(let i = 0; i < 20; i++){
             promises.push(crawlFirst(totalPage - i));
         }
-        // crawlFirst(totalPage - i)
-        // .then(results => {
-        //     // res.send(results[results.length - 1].post_time);
-        //     if(results[results.length - 1].post_time == '12/13'){
-        //         res.send(results);
-        //         // console.log(results);
-        //         // flag == false;
-        //     }
-        // })
-        // .catch(err => {
-        //     res.send(err);
-        // })
-
-        // }
-        // console.log(promises);
         
         Promise.all(promises)
         .then(results => {
-            // const arr = [];
-            // results.forEach(result => {
-            //     arr.concat(result);
-            // })
             const flattened_arr = [].concat(...results);
-            // res.send(flattened_arr);
             const promises = [];
             flattened_arr.forEach(url => {
                 promises.push(crawlPost(url));
@@ -45,7 +25,6 @@ exports.getFirstPage = (req, res, next) => {
             Promise.all(promises)
             .then(results => {
                 var newArray = results.filter(value => Object.keys(value).length !== 0)
-                // res.send(newArray);
                 res.send(newArray);
             })
             .catch(err => {
@@ -66,11 +45,9 @@ function crawlFirst(page = ""){
     const options = {
         method: "GET",
         url: `https://www.ptt.cc/bbs/Hiking/index${page}.html`,
-        // proxy: "https://" + proxy,
         headers: {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36" 
         },
-        // tunnel: false,
         transform: function(body){
             return cheerio.load(body);
         }
@@ -80,7 +57,6 @@ function crawlFirst(page = ""){
     .then($ => {
         const lastPageUrl = $('.wide').eq(1).attr('href');
         var totalPage = Number(lastPageUrl.match(/[\d]+/)[0]) + 1;
-        // console.log(totalPage);
         if(page == ""){
             return totalPage;
         }else{
@@ -89,18 +65,9 @@ function crawlFirst(page = ""){
                 const topicUrl = $(this).children('.title').children('a').attr('href');
                 const topicName = $(this).children('.title').children('a').text();
                 
-                const post_time = $(this).children('.meta').children('.date').text();
-                // console.log(post_time);
                 if(topicName.startsWith("[揪人]")){
-                    // var data = {
-                    //     topicName: topicName,
-                    //     topicUrl: "https://ptt.cc/" + topicUrl,
-                    //     post_time: post_time
-                    // };
                     var data = "https://www.ptt.cc" + topicUrl;
-                    // console.log(data);
                     datas.push(data);
-                    // title.push("https://ptt.cc/" + $(this).attr('href'));
                 }
             });
 
@@ -129,9 +96,7 @@ function crawlPost(url){
         const body = content.match(/\n(.*?\n)+--/);
 
         const depart_time = title.match(pattern).length == 0 ? body[0].match(pattern) : title.match(pattern)[0];
-        // const depart_time = ;
         const formated_depart_time = formatTime(depart_time);
-        // console.log(depart_time);
         const data = {
             ptt_id: url.match(/Hiking\/([\d\w\.]+).html/)[1],
             subject: title,
@@ -139,9 +104,7 @@ function crawlPost(url){
             description: body[0],
             ptt_url: url
         }
-        // if(JSON.stringify(data) !== "{}"){
         return data;
-        // }
     })
     .catch(err => {
         return err;
@@ -176,15 +139,11 @@ function formatTime(time){
         });
         if(moment().format('MM') - time[0] > 5){
             year = moment().add(1, 'year').format('YYYY');
-
-            // console.log(moment().format('MM'));
-            // console.log(time.join('/'));
         }else{
             year = moment().format('YYYY');
         }
 
         return year + "-" + time.join('-');
-        // console.log(time);
     }
 
 }
